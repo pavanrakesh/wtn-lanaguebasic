a7425753d5477c88e5632260328e3498749a0f1f
pavanrakesh <52190785+pavanrakesh@users.noreply.github.com>
2019-06-25 11:10:55 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,13 @@
+public class A
+{
+ public static void main(String []args)
+ {
+  System.out.println(args[0]);
+  System.out.println("Technologies");
+  System.out.println(args[1]);
+  System.out.println(args[2]);
+  System.out.println("Technologies");
+  System.out.println(args[3]);
+
+ }
+}
